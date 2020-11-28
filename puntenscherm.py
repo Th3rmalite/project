0aa30b6fe621ef50e9fee97005232595261d5b76
bituq <d.noorland9@hotmail.com>
2020-11-28 16:03:25 +0100

    hoi

--- puntenscherm.py
+++ puntenscherm.py
@@ -1,1 +1,9 @@
+import drawPlayers as dp
 
+def setup():
+    size(1080,720)
+    dp.get_players([("Vincent","zwart"),("beide Dylans","wit"),("beide Dylans","rood"),("beide Dylans","blauw")])
+
+def draw():
+    background(255)
+    dp.draw_player_info()
