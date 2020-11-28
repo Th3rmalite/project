5319f3c0dae67abad78d399a274a72c6e8535589
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2020-11-28 15:07:14 +0100

    yess goeie code export :)

--- puntenscherm.py
+++ puntenscherm.py
@@ -1,2 +1,9 @@
-# lol dit is niet eens python
-print("hello world")
+import drawPlayers as dp
+
+def setup():
+    size(1080,720)
+    dp.get_players([("Vincent","zwart"),("beide Dylans","wit"),("beide Dylans","rood"),("beide Dylans","blauw")])
+
+def draw():
+    background(255)
+    dp.draw_player_info()
