191426c576639537d9449c54e13094d5cbdd1593
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2020-11-28 16:23:36 +0100

    merge

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import invoerscherm
 import puntenscherm
 
 screenSize = [1080, 720]
+count = 1
 
 def settings():
     size(screenSize[0], screenSize[1])
 
 def setup():
-    #invoerscherm.setup()
-    puntenscherm.setup([("Vincent","zwart"),("beide Dylans","wit"),("beide Dylans","rood"),("beide Dylans","blauw")])
+    invoerscherm.setup()
+    puntenscherm.setup(lijstmetnamen)
+        
         
 def draw():
-    #invoerscherm.draw()
+    invoerscherm.draw()
     puntenscherm.draw()
