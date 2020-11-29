07bdc9b765bb9b15169c599bf8f0b55f5262e0cc
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2020-11-29 18:15:46 +0100

    poging

--- main.py
+++ main.py
@@ -2,21 +2,37 @@ import invoerscherm
 import puntenscherm
 screenSize = [1080, 720]
 count = 1
+state = "start"
+
 
 def settings():
-    size(screenSize[0], screenSize[1])
+    if state == "start":
+        size(screenSize[0], screenSize[1])
 
 def setup():
-    invoerscherm.setup()
-    lijstmetnamen = invoerscherm.getNames()
-    puntenscherm.setup([['Dylan', 'blue'], ['Vincent', 'red']])
+    global state
+    
+    if state == "start":
+        invoerscherm.setup()
         
 def draw():
+    global state
+    
+    if state == "start":
+        invoerscherm.draw()
+    elif state == "createGame":
+        puntenscherm.setup(invoerscherm.getNames())
+        print(invoerscherm.getNames())
+        
+        #state = "startGame"
+    elif state == "startGame":
+        puntenscherm.draw()
+    
     if not invoerscherm.d.navigationButtons[0].selected:
         invoerscherm.draw()
     else:
-        puntenscherm.draw()
-
+        state = "createGame"
+    print(state)
 def keyTyped():
     for i in range(4):
         invoerscherm.d.textInputs[i].addText(key)
