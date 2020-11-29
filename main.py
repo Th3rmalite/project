727bba923ce81e1f0feb63baf2fdeed88b7e9771
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2020-11-29 18:32:54 +0100

    oke

--- main.py
+++ main.py
@@ -9,31 +9,35 @@ def settings():
     if state == "start":
         size(screenSize[0], screenSize[1])
 
+
 def setup():
     global state
     
     if state == "start":
         invoerscherm.setup()
+      
         
 def draw():
     global state
     
     if state == "start":
         invoerscherm.draw()
+        
         if not invoerscherm.d.navigationButtons[0].selected:
             invoerscherm.draw()
         else:
             state = "createGame"
             
     elif state == "createGame":
-        puntenscherm.setup(invoerscherm.getNames())
-        
+        rectMode(LEFT)
+        #puntenscherm.setup(invoerscherm.getNames())
+        puntenscherm.setup([("Vincent","black"),("beide Dylans","white"),("beide Dylans","red"),("beide Dylans","blue")])
         state = "startGame"
         
     elif state == "startGame":
         puntenscherm.draw()
     
-    print(state)
+    
 def keyTyped():
     for i in range(4):
         invoerscherm.d.textInputs[i].addText(key)
