bbc23dee53a5dd278d42cfa369d9ff4682c4ea65
bituq <d.noorland9@hotmail.com>
2020-12-01 13:17:10 +0100

    hoi

--- drawPlayers.py
+++ drawPlayers.py
@@ -64,7 +64,7 @@ def draw_player_info():
     textSize(26)
     for idx,i in enumerate(players):
         get_points(i)
-        fill(50)
+        fill(90)
         rect(120, 60 + (cardHeight+20)*idx, cardWidth, cardHeight, 5)
         fill(255)
         text(i.name, 140, 100 + (cardHeight+20)*idx)
