8ce7602d951396cdeb558c0aeed523b8a03577e6
bituq <d.noorland9@hotmail.com>
2020-12-05 14:24:32 +0100

    h\

--- objects.py
+++ objects.py
@@ -3,18 +3,28 @@ screenSize = [1280, 720]
 class Attribute:
 
     def __init__(self):
+
         self.default = {
-            'border': '1px',
             'width': '50%',
             'height': '100%',
             'color': color(0,0,0),
-            'x': '',
-            'y': ''
+            'background-color': color(255,255,255),
+            'x': '0px',
+            'y': '0px',
+
+            'margin': '1px',
+
+            'border': '1px',
+            'border-color': color(0,0,0), 
+            
+            'padding': '1px',
+
+            'radius': '0px',
         }
 
         for key, value in self.default.items():
             if isinstance(value, str):
-                self.default[key] = self.formatConvert(key, value)
+                self.default[key] = self.formatForCalculations(key, value)
         
         self.get = self.default
     
@@ -33,15 +43,46 @@ class Attribute:
         if key in self.get:
             try:
                 if isinstance(value, str):
-                    self.get[key] = self.formatConvert(key, value)
+                    self.get[key] = self.multipleValues(key, value)
                 else:
                     self.get[key] = value
             except:
                 print(str(value) + ' is not a valid value for ' + str(key) + '.\nWill use default value of ' + str(self.default[key]) + ' instead.\n')
         else:
             print(str(key) + ' does not exist in Attributes.\n')
+
+    def multipleValues(self, key, string):
+        '''
+        If the string contains multiple values (ex: 'padding': '2px 1px 5px 10px'), return a string with the correct values.
+        '''
+        lis = string.split(' ')
+        newValue = {'left': '',
+                    'top': '',
+                    'right': '',
+                    'bottom': ''}
+
+        for index in range(len(lis)):
+            lis[index] = self.formatForCalculations(key, lis[index])
+
+        if len(lis) == 1:
+            return lis[0]
+        elif len(lis) == 2:
+            newValue['left'] = lis[0]
+            newValue['right'] = lis[0]
+            newValue['top'] = lis[1]
+            newValue['bottom'] = lis[1]
+        elif len(lis) == 4:
+            newValue['left'] = lis[0]
+            newValue['right'] = lis[1]
+            newValue['top'] = lis[2]
+            newValue['bottom'] = lis[3]
+        else:
+            raise ValueError
+        
+        return newValue
+
     
-    def formatConvert(self, key, string):
+    def formatForCalculations(self, key, string):
         '''
         Convert percentages and pixels to numbers that can be used for calculations.
 
@@ -57,8 +98,6 @@ class Attribute:
                     return t
                 elif 'px' in string:
                     return int(temp)
-                else:
-                    return ValueError
         return string
         
     def keyFormat(self, key, value):
@@ -70,6 +109,26 @@ class Attribute:
         elif key == 'height' or key == 'y':
             return screenSize[1] * (value/100)
 
+class Rectangle:
+
+    def __init__(self, dictionary):
+        self.attribute = Attribute()
+
+        for key, value in dictionary.items():
+            self[key] = value
+
+    def __setitem__(self, key, value):
+        self.attribute[key] = value
+
+    def __getitem__(self, key):
+        return self.attribute[key]
+    
+    def draw(self):
+        strokeWeight(self['border'])
+        stroke(self['border-color'])
+        fill(self['background-color'])
+        rect(self['x'], self['y'], self['width'], self['height'], self['radius'])
+
 class Table:
 
     def __init__(self, dictionary):
@@ -80,10 +139,6 @@ class Table:
 
     def __setitem__(self, key, value):
         self.attribute[key] = value
-    
+
     def __getitem__(self, key):
         return self.attribute[key]
-    
-    def draw(self):
-        fill(self['color'])
-        rect(self['x'], self['y'], self['width'], self['height'])
--- objectsScreen.py
+++ objectsScreen.py
@@ -1,20 +1,20 @@
 from objects import *
 
-print(locals())
-
-table = Table({
+rectangle = Rectangle({
         'width': '50%',
         'height': '70%',
         'x': '25px',
         'y': '20px',
-        'color': color(80,0,0)
+        'color': color(80,0,0),
+        'border': '15px 10px 25px 0px'
     })
 
 def settings():
     size(screenSize[0], screenSize[1])
 
 def setup():
-    pass
+    global rectangle
+    print(rectangle.attribute.get)
 
 def draw():
-    table.draw()
+    rectangle.draw()
