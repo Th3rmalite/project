5f8c098b12f0fb8319625203b4f174a1a332b89a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-07 19:19:14 +0100

    yo

--- Brentscherm.py
+++ Brentscherm.py
@@ -1,5 +1,5 @@
-spelers = 4
-colors = ['white','red']
+spelers = 3
+colors = ['white','red','black']
 def setup():
     size(1080,720)
     
@@ -45,55 +45,155 @@ def draw():
         textSize(35)
         image(bord,50,50,100,100)
         text('x32, 8 per speler',150,145)
+        
+        image(pion_wit,50,200,40,50)
+        image(koning_wit,45,250,50,60)
+        image(koningin_wit,45,310,50,50)
+        image(toren_wit,50,360,40,50)
+        image(paard_wit,50,410,40,50)
+
+        image(pion_rood,195,200,40,50)
+        image(koning_rood,190,250,50,60)
+        image(koningin_rood,190,310,50,50)
+        image(toren_rood,195,360,40,50)
+        image(paard_rood,195,410,40,50)
+
+        image(pion_blauw,340,200,40,50)
+        image(koning_blauw,335,250,50,60)
+        image(koningin_blauw,335,310,50,50)
+        image(toren_blauw,340,360,40,50)
+        image(paard_blauw,340,410,40,50)
+
+        image(pion_zwart,485,200,40,50)
+        image(koning_zwart,480,250,50,60)
+        image(koningin_zwart,480,310,50,50)
+        image(toren_zwart,485,360,40,50)
+        image(paard_zwart,485,410,40,50)
         if 'white' in colors:
-            image(pion_wit,50,200,40,50)
-            text('x6',100,245)
-            image(koning_wit,45,250,50,60)
-            text('x1',100,300)
-            image(koningin_wit,45,310,50,50)
-            text('x1',100,350)
-            image(toren_wit,50,360,40,50)
-            text('x2',100,400)
-            image(paard_wit,50,410,40,50)
-            text('x2',100,450)
+            text('x6',100,245) # pion_wit
+            text('x1',100,300) # koning_wit
+            text('x1',100,350) # koningin_wit
+            text('x2',100,400) # toren_wit
+            text('x2',100,450) # paard_wit
         if 'red' in colors:
-            image(pion_rood,195,200,40,50)
-            text('x6',240,245)
-            image(koning_rood,190,250,50,60)
-            text('x1',240,300)
-            image(koningin_rood,190,310,50,50)
-            text('x1',240,350)
-            image(toren_rood,195,360,40,50)
-            text('x2',240,400)
-            image(paard_rood,195,410,40,50)
-            text('x2',240,450)
+            text('x6',240,245) # pion_rood
+            text('x1',240,300) # koning_rood
+            text('x1',240,350) # koningin_rood
+            text('x2',240,400) # toren_rood
+            text('x2',240,450) # paard_rood
+        if 'blue' in colors:
+            text('x6',390,245) # pion_blauw
+            text('x1',390,300) # koning_blauw
+            text('x1',390,350) # koningin_blauw
+            text('x2',390,400) # toren_blauw
+            text('x2',390,450) # paard_blauw
+        if 'black' in colors:
+            text('x6',535,245) # pion_zwart
+            text('x1',535,300) # koning_zwart
+            text('x1',535,350) # koningin_zwart
+            text('x2',535,400) # toren_zwart
+            text('x2',535,450) # paard_zwart
     if spelers == 3:
         textSize(35)
         image(bord,50,50,100,100)
         text('x27, 9 per speler',150,145)
+        
+        image(pion_wit,50,200,40,50)
+        image(koning_wit,45,250,50,60)
+        image(koningin_wit,45,310,50,50)
+        image(toren_wit,50,360,40,50)
+        image(paard_wit,50,410,40,50)
+
+        image(pion_rood,195,200,40,50)
+        image(koning_rood,190,250,50,60)
+        image(koningin_rood,190,310,50,50)
+        image(toren_rood,195,360,40,50)
+        image(paard_rood,195,410,40,50)
+
+        image(pion_blauw,340,200,40,50)
+        image(koning_blauw,335,250,50,60)
+        image(koningin_blauw,335,310,50,50)
+        image(toren_blauw,340,360,40,50)
+        image(paard_blauw,340,410,40,50)
+
+        image(pion_zwart,485,200,40,50)
+        image(koning_zwart,480,250,50,60)
+        image(koningin_zwart,480,310,50,50)
+        image(toren_zwart,485,360,40,50)
+        image(paard_zwart,485,410,40,50)
         if 'white' in colors:
-            image(pion_wit,50,200,40,50)
-            text('x18, 6 per speler',100,245)
-            image(koning_wit,45,250,50,60)
-            text('x3, 1 per speler',100,300)
-            image(koningin_wit,45,310,50,50)
-            text('x3, 1 per speler',100,350)
-            image(toren_wit,50,360,40,50)
-            text('x6, 2 per speler',100,400)
-            image(paard_wit,50,410,40,50)
-            text('x6, 2 per speler',100,450)
+            text('x6',100,245) # pion_wit
+            text('x1',100,300) # koning_wit
+            text('x1',100,350) # koningin_wit
+            text('x2',100,400) # toren_wit
+            text('x2',100,450) # paard_wit
+        if 'red' in colors:
+            text('x6',240,245) # pion_rood
+            text('x1',240,300) # koning_rood
+            text('x1',240,350) # koningin_rood
+            text('x2',240,400) # toren_rood
+            text('x2',240,450) # paard_rood
+        if 'blue' in colors:
+            text('x6',390,245) # pion_blauw
+            text('x1',390,300) # koning_blauw
+            text('x1',390,350) # koningin_blauw
+            text('x2',390,400) # toren_blauw
+            text('x2',390,450) # paard_blauw
+        if 'black' in colors:
+            text('x6',535,245) # pion_zwart
+            text('x1',535,300) # koning_zwart
+            text('x1',535,350) # koningin_zwart
+            text('x2',535,400) # toren_zwart
+            text('x2',535,450) # paard_zwart
     if spelers == 2:
         textSize(35)
         image(bord,50,50,100,100)
         text('x20, 10 per speler',150,145)
+        
+        image(pion_wit,50,200,40,50)
+        image(koning_wit,45,250,50,60)
+        image(koningin_wit,45,310,50,50)
+        image(toren_wit,50,360,40,50)
+        image(paard_wit,50,410,40,50)
+
+        image(pion_rood,195,200,40,50)
+        image(koning_rood,190,250,50,60)
+        image(koningin_rood,190,310,50,50)
+        image(toren_rood,195,360,40,50)
+        image(paard_rood,195,410,40,50)
+
+        image(pion_blauw,340,200,40,50)
+        image(koning_blauw,335,250,50,60)
+        image(koningin_blauw,335,310,50,50)
+        image(toren_blauw,340,360,40,50)
+        image(paard_blauw,340,410,40,50)
+
+        image(pion_zwart,485,200,40,50)
+        image(koning_zwart,480,250,50,60)
+        image(koningin_zwart,480,310,50,50)
+        image(toren_zwart,485,360,40,50)
+        image(paard_zwart,485,410,40,50)
         if 'white' in colors:
-            image(pion_wit,50,200,40,50)
-            text('x12, 6 per speler',100,245)
-            image(koning_wit,45,250,50,60)
-            text('x2, 1 per speler',100,300)
-            image(koningin_wit,45,310,50,50)
-            text('x2, 1 per speler',100,350)
-            image(toren_wit,50,360,40,50)
-            text('x4, 2 per speler',100,400)
-            image(paard_wit,50,410,40,50)
-            text('x4, 2 per speler',100,450)
+            text('x6',100,245) # pion_wit
+            text('x1',100,300) # koning_wit
+            text('x1',100,350) # koningin_wit
+            text('x2',100,400) # toren_wit
+            text('x2',100,450) # paard_wit
+        if 'red' in colors:
+            text('x6',240,245) # pion_rood
+            text('x1',240,300) # koning_rood
+            text('x1',240,350) # koningin_rood
+            text('x2',240,400) # toren_rood
+            text('x2',240,450) # paard_rood
+        if 'blue' in colors:
+            text('x6',390,245) # pion_blauw
+            text('x1',390,300) # koning_blauw
+            text('x1',390,350) # koningin_blauw
+            text('x2',390,400) # toren_blauw
+            text('x2',390,450) # paard_blauw
+        if 'black' in colors:
+            text('x6',535,245) # pion_zwart
+            text('x1',535,300) # koning_zwart
+            text('x1',535,350) # koningin_zwart
+            text('x2',535,400) # toren_zwart
+            text('x2',535,450) # paard_zwart
