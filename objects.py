c5ccd0d0ce98ce4b505ab1b150081883d3c84d38
bituq <d.noorland9@hotmail.com>
2020-12-15 10:26:48 +0100

    new

--- objects.py
+++ objects.py
@@ -1,261 +1,114 @@
-Screen = {'width': 1280, 'height': 720, 'x': 0, 'y': 0}
-screenSize = [1280, 720]
+# Set properties
+# Get properties
+# Convert % and px to correct format
+# Format multiple values to work in practice
 
-content = []
+screen = {
+        'w': 1280,
+        'h': 720
+    }
 
-class Properties:
+class Property:
+    global screen
 
-    def __init__(self, parent):
-        global Screen
+    def __init__(self, parent = None):
         if not parent:
-            self.parent = Screen
+            self.parent = screen
         else:
             self.parent = parent
 
         self.default = {
-            'width': '50px',
-            'height': '50px',
-            'color': color(0,0,0),
-            'background-color': color(255,255,255),
-            'x': '0px',
-            'y': '0px',
+                'w': '50%',
+                'h': '50px',
+                'x': 0,
+                'y': 0
+            }
 
-            'border': '1px',
-            'border-color': color(0,0,0), 
+        self.storage = {}
 
-            'radius': '0px',
+        self.setDefault()
 
-            'inherit': 'absolute',
-            'text-align': 'left',
-            'vertical-align': 'top',
-            'rect-align': CORNER,
+    def setDefault(self, copyValues = True):
+        if copyValues == True:
+            self.storage = self.default
+        else:
+            for propertyType in self.default.keys():
+                self.storage.update({propertyType: ''})
+
+    def __setitem__(self, propertyType, propertyValue):
+        if propertyType in self.default.keys():
+            if not isinstance(propertyValue, str):
+                self.storage[propertyType] = self.toNormal(propertyValue)
+            else:
+                self.storage[propertyType] = propertyValue
+        else:
+            print("'" + str(propertyType) + "' is not a valid property key!")
             
-            'box-shadow': 'none',
-            'shadow-color': color(0,0,0,2)
-        }
 
-        self.get = self.default
+    def __getitem__(self, propertyType, realValue = False):
+        if realValue == True:
+            return self.toReal(propertyType, self.storage[propertyType])
+        else:
+            return self.storage[propertyType]
 
-        for key, value in self.default.items():
-            if isinstance(value, str):
-                try:
-                    self.default[key] = self.multipleValues(key, value)
-                except:
-                    print('<An error has occured> ' + key + ' : ' + value)
-            else:
-                self.default[key] = value
-        
-        self.get = self.default
+    def setProperties(self, properties):
+        for propertyKey, propertyType in properties.items():
+            self[propertyKey] = propertyType
     
+    def getProperties(self):
+        temp = {}
+        for propertyKey in self.storage.keys():
+            temp[propertyKey] = str(self[propertyKey])
+        return temp
+
+    def setMultipleItems(self):
+        pass
+
+    def toReal(self, propertyType, normalValue):
+        if isinstance(normalValue, int):
+            return self.toReal(propertyType, self.toNormal(normalValue))
+        elif 'px' in normalValue:
+            return int(normalValue.replace('px', ''))
+        elif '%' in normalValue:
+            percentageToPixels = int(self.parent[propertyType] * (float(normalValue.replace('%', '')) / 100))
+            return self.toReal(propertyType, self.toNormal(percentageToPixels))
+
+    def toNormal(self, realValue):
+        try:
+            return str(int(realValue)) + 'px'
+        except:
+            print('Could not turn ' + str(realValue) + ' into normal value. Probably because the value is not a number.')
+
+class FormObject(object):
+    global screen
+
+    def __init__(self, parent, properties):
+        self.properties = Property(parent)
+        self.properties.setProperties(properties)
+
+    def __setitem__(self, propertyType, propertyValue):
+        self.properties[propertyType] = propertyValue
+
+    def __getitem__(self, propertyType):
+        return self.properties[propertyType]
+
+    def onHover(self):
+        pass
+
+    def onClick(self):
+        pass
+
+class Screen(FormObject):
+
+    def __init__(self, properties):
+        FormObject.__init__(None, properties)
+        self.content = {}
+    
+    def addContent(self, key, value):
+        self.content[key] = value
+    
+    def delContent(self, key):
+        del self.content[key]
+
     def __getitem__(self, key):
-        '''
-        Returns the table with all current Properties for the object.
-        '''
-        return self.get[key]
-
-    def __setitem__(self, key, value):
-        '''
-        Change the Properties of an object easily:
-            <objName>[keyName] = value
-            table['height'] = '50px'
-        '''
-        if key in self.get:
-            try:
-                if isinstance(value, str):
-                    self.get[key] = self.multipleValues(key, value)
-                else:
-                    self.get[key] = value
-            except:
-                print(str(value) + ' is not a valid value for ' + str(key) + '.\nWill use default value of ' + str(self.default[key]) + ' instead.\n')
-        else:
-            print(str(key) + ' does not exist in Propertiess.\n')
-
-    def multipleValues(self, key, string):
-        '''
-        If the string contains multiple values (ex: 'padding': '2px 1px 5px 10px'), return a string with the correct values.
-        '''
-        lis = string.split(' ')
-        newValue = {}
-
-        for index in range(len(lis)):
-            lis[index] = self.formatForCalculations(key, lis[index])
-        
-        if key == 'box-shadow':
-            if len(lis) == 1:
-                return 'none'
-            elif len(lis) == 2:
-                newValue = {
-                    'offset-x' : lis[0],
-                    'offset-y' : lis[1],
-                    'spread' : 0
-                }
-            elif len(lis) == 3:
-                newValue = {
-                    'offset-x' : lis[0],
-                    'offset-y' : lis[1],
-                    'spread' : lis[2]
-                }
-        else:
-            if len(lis) == 1:
-                if key != 'radius':
-                    return lis[0]
-                else:
-                    newValue = {
-                        'left' : lis[0],
-                        'top' : lis[0],
-                        'right' : lis[0],
-                        'bottom' : lis[0]
-                    }
-            elif len(lis) == 2:
-                newValue = {
-                        'left' : lis[0],
-                        'top' : lis[1],
-                        'right' : lis[0],
-                        'bottom' : lis[1]
-                    }
-            elif len(lis) == 4:
-                newValue = {
-                        'left' : lis[0],
-                        'top' : lis[1],
-                        'right' : lis[2],
-                        'bottom' : lis[3]
-                    }
-        
-        return newValue
-
-    
-    def formatForCalculations(self, key, string):
-        '''
-        Convert percentages and pixels to numbers that can be used for calculations.
-
-        Returns a ValueError when the string doesn't match either.
-        '''
-        temp = ''
-        for character in string:
-            if character.isnumeric():
-                temp += character
-            else:
-                if '%' in string:
-                    t = floor(self.keyFormat(key, float(temp)))
-                    return t
-                elif 'px' in string:
-                    return self.otherKeyFormat(key, temp)
-        return string
-        
-    def keyFormat(self, key, value):
-        '''
-        Division for percentages.
-        '''
-        if self['inherit'] == 'absolute':
-            if key == 'width' or key == 'x':
-                return Screen['width'] * (value/100)
-            elif key == 'height' or key == 'y':
-                return Screen['height'] * (value/100)
-        elif self['inherit'] == 'relative':
-            if key == 'width':
-                return self.parent['width'] * (value/100)
-            elif key == 'height':
-                return self.parent['height'] * (value/100)
-            if self.parent['rect-align'] == CORNER:
-                if key == 'x':
-                    return self.parent['x'] + self.parent['width'] * (value/100)
-                elif key == 'y':
-                    return self.parent['y'] + self.parent['height'] * (value/100)
-            elif self.parent['rect-align'] == CENTER:
-                if key == 'x':
-                    return self.parent['x'] + self.parent['width'] * (value/100) - self.parent['width'] / 2
-                elif key == 'y':
-                    return self.parent['y'] + self.parent['height'] * (value/100) - self.parent['height'] / 2
- 
-    def otherKeyFormat(self, key, value):
-        '''
-        Used for calculating the object's screen position relative to the position of the parent.
-        '''
-        if self['inherit'] == 'absolute':
-            return int(value)
-        elif self['inherit'] == 'relative':
-            if self.parent['rect-align'] == CORNER:
-                if key == 'x':
-                    return self.parent['x'] + int(value)
-                elif key == 'y':
-                    return self.parent['y'] + int(value)
-            else:
-                if key == 'x':
-                    return self.parent['x'] - self.parent['width'] / 2 + int(value)
-                elif key == 'y':
-                    return self.parent['y'] - self.parent['height'] / 2 + int(value)
-
-class formObject(object):
-    
-    def __init__(self, parent, dictionary):
-        self.properties = Properties(parent)
-
-        for key, value in dictionary.items():
-            self[key] = value
-        
-        self.parent = parent
-        global content
-        content.append(self)
-    
-    def __setitem__(self, key, value):
-        self.properties[key] = value
-    
-    def __getitem__(self, key):
-        return self.properties[key]
-
-    def drawShadow(self):
-        if self['box-shadow'] != 'none':
-            if self['rect-align'] == CORNER:
-                rectMode(CENTER)
-                noStroke()
-                fill(self['shadow-color'])
-                for i in range(120):
-                    rect(
-                        (self['x'] + self['width'] / 2) + self['box-shadow']['offset-x'],
-                        (self['y'] + self['height'] / 2) + self['box-shadow']['offset-y'],
-                        self['width'] - i * .5 + self['box-shadow']['spread'],
-                        self['height'] - i * .5 + self['box-shadow']['spread'],
-                        self['radius']['left'] + 5,
-                        self['radius']['top'] + 5,
-                        self['radius']['right'] + 5,
-                        self['radius']['bottom'] + 5
-                        )
-            elif self['rect-align'] == CENTER:
-                rectMode(CENTER)
-                noStroke()
-                fill(self['shadow-color'])
-                for i in range(120):
-                    rect(
-                        self['x'] + self['box-shadow']['offset-x'],
-                        self['y'] + self['box-shadow']['offset-y'],
-                        self['width'] - i * .5 + self['box-shadow']['spread'],
-                        self['height'] - i * .5 + self['box-shadow']['spread'],
-                        self['radius']['left'] + 5,
-                        self['radius']['top'] + 5,
-                        self['radius']['right'] + 5,
-                        self['radius']['bottom'] + 5
-                        )
-
-
-class Rectangle(formObject):
-
-    def __init__(self, parent, dictionary):
-        formObject.__init__(self, parent, dictionary)
-
-    
-    def draw(self):
-        if self['border'] == 0:
-            noStroke()
-        else:
-            strokeWeight(self['border'])
-            stroke(self['border-color'])
-        fill(self['background-color'])
-        rectMode(self['rect-align'])
-        rect(self['x'], self['y'], self['width'], self['height'], self['radius']['left'], self['radius']['top'], self['radius']['right'], self['radius']['bottom'])
-
-class Table(formObject):
-
-    def __init__(self, parent, dictionary):
-        formObject.__init__(self, parent, dictionary)
-    
-    
+        return self.content[key]
--- objectsScreen.py
+++ objectsScreen.py
@@ -1,56 +1,25 @@
 from objects import *
-import time
 
-Background = Rectangle(False, {
-    'width': '100%',
-    'height': '100%',
-    'background-color': color(255,255,255),
-    'border': '0px'
-})
+# Definieer alle objecten in een scherm. Je kan deze dupliceren en aanpassen. 
+# Hier kan je ook aanpassen wat er gebeurt als je ergens overheen gaat met je muis.
 
-Table = Rectangle(False, {
-    'rect-align': CENTER,
-    'width': '40%',
-    'height': '80%',
-    'background-color': color(218, 214, 214),
-    'x': '50%',
-    'y': '50%',
-    'border': '0px',
-    'box-shadow': '5px 5px 3px'
-})
-
-Row1 = Rectangle(Table, {
-    'rect-align': CORNER,
-    'inherit': 'relative',
-    'width': '100%',
-    'height': '25%',
-    'x': '0px',
-    'y': '75%',
-    'border': '0px',
-    'box-shadow': '0px 5px 5px',
-    'background-color': color(146, 191, 177)
-})
-
-Row2 = Rectangle(Table, {
-    'rect-align': CORNER,
-    'inherit': 'relative',
-    'width': '100%',
-    'height': '25%',
-    'x': '0px',
-    'y': '50%',
-    'border': '0px',
-    'box-shadow': '0px 5px 5px',
-    'background-color': color(244, 172, 69)
+objectScreen = Screen({
+    'background-color': rgb(255,255,255)
 })
 
 def settings():
-    size(screenSize[0], screenSize[1])
+    size(screen['w'], screen['h'])
 
 def setup():
-    pass
-    
+    test = FormObject(None,{
+        'w': '50%',
+        'h': '10%'
+    })
+    print(test.properties.getProperties())
+    test['h'] = 50
+    print(test['h'])
+    test['h'] = '20%'
+    print(test['h'])
+
 def draw():
-    global i
-    for index in range(len(content)):
-        content[index].drawShadow()
-        content[index].draw()
+    pass
