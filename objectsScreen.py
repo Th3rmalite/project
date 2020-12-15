c4c2acbb4a88544d474866d3b6dee41cc8dbf307
bituq <d.noorland9@hotmail.com>
2020-12-15 10:50:26 +0100

    new

--- objectsScreen.py
+++ objectsScreen.py
@@ -3,9 +3,12 @@ from objects import *
 # Definieer alle objecten in een scherm. Je kan deze dupliceren en aanpassen. 
 # Hier kan je ook aanpassen wat er gebeurt als je ergens overheen gaat met je muis.
 
-objectScreen = Screen({
-    'background-color': rgb(255,255,255)
-})
+puntenScherm = Screen({})
+namenScherm = Screen({})
+menuScherm = Screen({})
+
+namenScherm.addContent({'speler 1': 'blue','speler 2': 'red', 'speler 3': 'black', 'speler 4': 'white'})
+puntenScherm.addContent(namenScherm.content)
 
 def settings():
     size(screen['w'], screen['h'])
