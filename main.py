b2472dc0e0a5af7288bf2c195f18e95ca14883da
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2021-01-11 12:27:52 +0100

    yeet

--- main.py
+++ main.py
@@ -36,13 +36,14 @@ def draw():
         
     elif state == "startGame":
         puntenscherm.draw()
-        if len(getAlivePlayers()) == 1:
-            state == "endGame"
-    elif state == "endGame":
-        pass
+        if len(puntenscherm.dp.getAlivePlayers()) == 1:
+            state = "endGameSetup"
     
-    elif state == "endGame":
+    elif state == "endGameSetup":
         eindscherm.setup()
+        state = "endGame"
+    elif state == "endGame":
+        eindscherm.draw()
     
 def keyTyped():
     for i in range(4):
