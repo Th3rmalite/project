00458c0ccd38cb8e9e718553a40ade7154ac1e38
Vincent <43859503+Th3rmalite@users.noreply.github.com>
2021-01-13 16:43:15 +0100

    pls niet

--- uitleg.py
+++ uitleg.py
@@ -1,6 +1,6 @@
 offset = 0
 mul = 2
-message = "move the pieces to the player that captured it"
+message = "Sleep het speelstuk naar jouw vak wanneer het is geslagen"
 piece = loadImage("pawn.png")
 cursor = loadImage("mouse.png")
 cursor.resize(100, 100)
@@ -25,5 +25,7 @@ def draw():
     image(piece, 450, 180 + offset)
     image(cursor, 500, 210 + offset)
     text(message, 540, 100)
+    textFont(loadFont('OpenSans-Bold-48.vlw'), 12)
+    text('(klik op "ga verder" om naar het volgende scherm te gaan)', 540, 120)
     
     offset += mul
